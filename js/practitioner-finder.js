612b2e4414ab768b582d7b49220068319927c2d4
Hoa <hoa.truong@baselgovernance.org>
2015-03-06 11:22:19 +0100

    add zoomin+out for buttons, but not working yet

--- js/practitioner-finder.js
+++ js/practitioner-finder.js
@@ -1,290 +1,213 @@
-/**
- * Created by hoa on 05/03/15.
- */
+d3.select(window).on("resize", throttle);
 
-//queue
-(function(){function n(n){function t(){for(;f=a<c.length&&n>p;){var u=a++,t=c[u],r=l.call(t,1);r.push(e(u)),++p,t[0].apply(null,r)}}function e(n){return function(u,l){--p,null==d&&(null!=u?(d=u,a=s=0/0,r()):(c[n]=l,--s?f||t():r()))}}function r(){null!=d?v(d):i?v(d,c):v.apply(null,[d].concat(c))}var o,f,i,c=[],a=0,p=0,s=0,d=null,v=u;return n||(n=1/0),o={defer:function(){return d||(c.push(arguments),++s,t()),o},await:function(n){return v=n,i=!1,s||r(),o},awaitAll:function(n){return v=n,i=!0,s||r(),o}}}function u(){}"undefined"==typeof module?self.queue=n:module.exports=n,n.version="1.0.4";var l=[].slice})();
+//Zoom map by wheel
+var zoom = d3.behavior.zoom()
+    .scaleExtent([1, 8]) //range of zoom scale as [minimum, maximum].
+    .on("zoom", zoomed);
 
 
+console.log(zoom);
 
-var width = document.getElementById('map').offsetWidth-5;
-var height = width * 2/3;
+//Zoom map by buttons
 
-var instruction = "Use zoom and pan tools to find a Asset Recovery Practitioner or simple use the below drop down menu to find by country";
 
-var topo,projection,path,svg,g,all,cdata,ccapitals,
-    current = 16; //current country, randomly choosen number
+var width = document.getElementById('map').offsetWidth-1;
+var height =  width * 2/3 ;
 
-var tooltip = d3.select("body").append("div").attr("class", "tooltip hidden");
+var topo,projection,path,svg,g;
+
+//var color = d3.scale.quantize()
+//    .range(["rgb(237,248,233)", "rgb(186,228,179)",
+//        "rgb(116,196,118)", "rgb(49,163,84)","rgb(0,109,44)"]);
+var mapColor = "#ccb9b4";
+var biogRed = "#93173B" ;
+
+var instruction = "Use zoom and pan tools to find a Asset Recovery Practitioner or simply use the below drop down menu to find by country";
+
+var tooltip = d3.select("#map").append("div").attr("class", "tooltip hidden");
 
 setup(width,height);
 
 function setup(width,height){
+    projection = d3.geo.mercator()
+        .translate([0, 0])
+        .scale(width / 2 / Math.PI);
 
-    projection = d3.geo.mercator().translate([0, 0]).scale(width / 2 / Math.PI);
-
-    path = d3.geo.path().projection(projection);
+    path = d3.geo.path()
+        .projection(projection);
 
     svg = d3.select("#map").append("svg")
         .attr("width", width)
-        .attr("height", height);
-
-    svg.append("rect")
-        .attr("width", width)
         .attr("height", height)
-        .style("fill","#fff")
-        .on("click", loadworldmap);
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+        .call(zoom);
 
-    var outterg = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    g = svg.append("g");
 
-    g = outterg.append("g").attr("id", "innerg");
-
-    //setup next and previous links
-    d3.select("#next").on("click",function(){
-        var next = current + 1;
-        if(cdata[next] !== undefined){
-            loadit(cdata[next]);
-            current++;
-        }
-    });
-
-    d3.select("#previous").on("click",function(){
-        if(current == 0){
-            var previous = all-1;
-        } else {
-            var previous = current-1;
-        }
-        if(cdata[previous] !== undefined){
-            loadit(cdata[previous]);
-            current--;
-        }
-    });
+    d3.select("#instruction").text(instruction);
 
 }
 
+d3.json("data/world-topo.json", function(error, world) {
 
-queue()
-    .defer(d3.csv, "data/countries-data.csv")
-    .defer(d3.json, "data/capitals.json")
-    .await(ready);
+    var countries = topojson.feature(world, world.objects.countries).features;
 
-function ready(error, countries, capitals) {
-    cdata = countries;
-    ccapitals = capitals;
-
-    loadworldmap();
+    topo = countries;
+    draw(topo);
 
 
-    //country dropdown
-    var options = '';
-    cdata.forEach(function(d) {
-        options += '<option value="'+d.iso_a3+'">'+d.name+'</option>';
+});
+
+function draw(topo) {
+
+    var country = g.selectAll(".country").data(topo);
+
+    country.enter().insert("path")
+        .attr("class", "country")
+        .attr("d", path)
+        .attr("id", function(d,i) { return d.id; })
+        .attr("title", function(d,i) { return d.properties.name; })
+        .style("fill", mapColor );
+        // default colors
+        //.style("fill", function(d, i) { return  d.properties.color; });
+
+    //ofsets plus width/height of transform, plsu 20 px of padding, plus 20 extra for tooltip offset off mouse
+    var offsetL = document.getElementById('container').offsetLeft+(width/2)+40;
+    var offsetT =document.getElementById('container').offsetTop+(height/2)+20;
+
+    //tooltips
+    country
+        .on("mousemove", function(d,i) {
+            var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+            tooltip
+                .classed("hidden", false)
+                .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
+                .html(d.properties.name)
+        })
+        .on("mouseout",  function(d,i) {
+            tooltip.classed("hidden", true)
+        });
+
+}
+
+function redraw() {
+    width = document.getElementById('map').offsetWidth-1;
+    height = width / 2;
+    d3.select('svg').remove();
+    setup(width,height);
+    draw(topo);
+}
+
+//To zoom by wheel
+function zoomed() {
+    //svg.attr("transform",
+    //    "translate(" + zoom.translate() + ")" +
+    //    "scale(" + zoom.scale() + ")"
+    //);
+
+    //BELOW IS WORKING CODE, BUT WANT TO TEST BUSSTOCK EXAMPLE ON ZOOM
+    var t = d3.event.translate;
+    var s = d3.event.scale;
+    var h = height / 3;
+
+    t[0] = Math.min(width / 2 * (s - 1), Math.max(width / 2 * (1 - s), t[0]));
+    t[1] = Math.min(height / 2 * (s - 1) + h * s, Math.max(height / 2 * (1 - s) - h * s, t[1]));
+
+    zoom.translate(t);
+    g.style("stroke-width", 1 / s).attr("transform", "translate(" + t + ")scale(" + s + ")");
+
+}
+function interpolateZoom (translate, scale) {
+    var self = this;
+    return d3.transition().duration(350).tween("zoom", function () {
+        var iTranslate = d3.interpolate(zoom.translate(), translate),
+            iScale = d3.interpolate(zoom.scale(), scale);
+        return function (t) {
+            zoom
+                .scale(iScale(t))
+                .translate(iTranslate(t));
+            zoomed();
+        };
     });
+}
 
-    d3.select("#country").html(options)
-        .on("change", function() {
-            var sel = this.value;
-            var obj = cdata.filter(function(f){return f.iso_a3 == sel;})[0];
-            loadit(obj);
-        });
+function zoomClick() {
+    var clicked = d3.event.target,
+        direction = 1,
+        factor = 0.2,
+        target_zoom = 1,
+        center = [width / 2, height / 2],
+        extent = zoom.scaleExtent(),
+        translate = zoom.translate(),
+        translate0 = [],
+        l = [],
+        view = {x: translate[0], y: translate[1], k: zoom.scale()};
 
-    //color countries and print country names to info box
-    function colorandprint(objs,title){
-        d3.selectAll('.country').style('fill','#ccc'); //clear
+    d3.event.preventDefault();
+    direction = (this.id === 'zoom_in') ? 1 : -1;
+    target_zoom = zoom.scale() * (1 + factor * direction);
 
-        var html = '<h2>Category: '+title+'</h2><br><br>';
+    if (target_zoom < extent[0] || target_zoom > extent[1]) { return false; }
 
-        objs.forEach(function(f){
-            d3.select('.country[title="'+f.name+'"]').style('fill','#666')
-            html += f.name+'<br>';
-        });
+    translate0 = [(center[0] - view.x) / view.k, (center[1] - view.y) / view.k];
+    view.k = target_zoom;
+    l = [translate0[0] * view.k + view.x, translate0[1] * view.k + view.y];
 
-        d3.select("#info").html(html);
+    view.x += center[0] - l[0];
+    view.y += center[1] - l[1];
+
+    interpolateZoom([view.x, view.y], view.k);
+}
+
+//d3.selectAll('button').on('click', zoomClick);
+
+
+
+
+// To zoom by buttons
+function zoomBtn(action, mapFeatures) {
+    var currentZoom = zoom.scale();
+    var options = zoom.scaleExtent();
+
+    //D3 zooms the center of svg, not at top left
+    //Therefore, we need to zeroing svg, stretching it, then unzero again
+
+    //x = (x - center[0]) * factor + center[0];
+    //y = (y - center[1]) * factor + center[1];
+
+    if( action == 'in' ){
+        if(currentZoom < options.maxZoomLevel){
+            var newScale = Math.floor(currentZoom) + 1;
+
+            var b = path.bounds(mapFeatures);
+            var t = [(width - newScale * (b[1][0] + b[0][0])) / 2, (height - newScale * (b[1][1] + b[0][1])) / 2];
+
+            zoom.scale(newScale)
+                .translate(t)
+                .event(svg);
+        }
+    }else{
+        if(currentZoom > options.minZoomLevel){
+            var newScale = Math.floor(currentZoom) - 1;
+
+            var b = path.bounds(mapFeatures);
+            var t = [(width - newScale * (b[1][0] + b[0][0])) / 2, (height - newScale * (b[1][1] + b[0][1])) / 2];
+
+            zoom.scale(newScale)
+                .translate(t)
+                .event(svg);
+        }
     }
-
-    //income dropdown
-    d3.select("#income").on("change", function() {
-
-        d3.select("#economy")[0][0].selectedIndex = 0;
-
-        var sel = this.value;
-        if(sel !== 0){
-            var objs = cdata.filter(function(f){return f.income_grp == sel});
-            colorandprint(objs,sel);
-        }
-
-    });
-
-    //economy dropdown
-    d3.select("#economy").on("change", function() {
-
-        d3.select("#income")[0][0].selectedIndex = 0;
-
-        var sel = this.value;
-        if(sel !== 0){
-            var objs = cdata.filter(function(f){return f.economy == sel});
-            colorandprint(objs,sel);
-        }
-
-    });
-
 }
 
 
-function loadworldmap() {
-
-    clear();
-    g.style("stroke-width", 1).attr("transform", "");
-    d3.select("#next").style("visibility","hidden");
-    d3.select("#previous").style("visibility","hidden");
-
-    d3.json("data/world-topo-110m.json", function(error, world) {
-
-        var topo = topojson.feature(world, world.objects.countries).features;
-
-        var country = d3.select("#innerg").selectAll(".country").data(topo);
-
-
-        //Tooltip country name
-        country.enter().insert("path")
-            .attr("class", "country")
-            .attr("d", path)
-            .attr("id", function(d,i) { return d.id; })
-            .attr("title", function(d,i) { return d.properties.name; })
-            .style("fill", "#ccc")
-            .style("stroke", "#111")
-            .on("click", click)
-           .on("mousemove", mouseover)
-            .on("mouseout",  function(d,i) {
-                tooltip.classed("hidden", true);
-            });
-
-       d3.select("#instruction").text(instruction);
-
-    });
-
+var throttleTimer;
+function throttle() {
+    window.clearTimeout(throttleTimer);
+    throttleTimer = window.setTimeout(function() {
+        redraw();
+    }, 200);
 }
 
-function mouseover(d,i) {
-    //Tooltip offsets away from mouse pointer
-    var offsetL = document.getElementById('map').offsetLeft+30;
-    var offsetT = document.getElementById('map').offsetTop-30;
-    var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
-
-    //print tooltip country name
-    tooltip.classed("hidden", false)
-        .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
-        .html(d.properties.name);
-
-}
-
-function click(d) {
-    //click going to the country
-    var obj = cdata.filter(function(f){return f.iso_n3 == d.id})[0];
-    loadit(obj);
-}
-
-
-function loadit(countryobj) {
-
-    tooltip.attr("class","tooltip hidden");
-    d3.select("#next").style("visibility","");
-    d3.select("#previous").style("visibility","");
-
-    var name = countryobj.name;
-    var code = countryobj.iso_a3;
-    var filename = "data/countries-10m-topojson/"+code+".json";
-
-    d3.json(filename, function(error, country) {
-
-        var obj = topojson.feature(country, country.objects[code]);
-
-        clear();
-
-        var b = path.bounds(obj);
-        var s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
-
-
-        //Center the country to the middle of the map
-        g.style("stroke-width", 1 / s).attr("transform", "scale(" + s + ")" +
-        "translate(" + -(b[1][0] + b[0][0]) / 2 + "," + -(b[1][1] + b[0][1]) / 2 + ")");
-
-
-        d3.select("#innerg").append("path").datum(obj)
-            .style("fill","#ccc")
-            .style("stroke", "#111")
-            .attr("d", path);
-
-        d3.select("#instruction").text(name);
-
-        //data object to html
-        var html = '';
-
-        for (var o in countryobj) {
-            if (countryobj.hasOwnProperty(o)) {
-                var label = o;
-                var value = countryobj[o];
-                var info = '<div><label>'+label+'</label><span>'+value+'</span></div>';
-                html += info; //with formatting
-            }
-        }
-
-        d3.select("#info").html(html);
-
-        //find the capital
-        var cap = ccapitals.filter(function(f){ if(f.country == name){ return f; }  });
-        if(cap.length > 0){
-            var capital = cap[0].capital;
-            var lat = cap[0].lat;
-            var lon = cap[0].lon;
-            var lonlat = [lon,lat]; //reversed for d3
-
-            //plot the capital
-            var x = projection(lonlat)[0];
-            var y = projection(lonlat)[1];
-
-            var size = (1/s * 30);
-            var p = (1/s * 10);
-
-            g.append("svg:circle")
-                .attr("class","point")
-                .attr("cx", x)
-                .attr("cy", y)
-                .attr("r", p)
-                .style("fill","#000");
-
-            g.append("text")
-                .attr("x", x+.5)
-                .attr("y", y)
-                .style("font-size", size)
-                .text(capital);
-
-        }
-
-
-    });
-
-
-}
-
-function clear(){
-    //clear shit
-    d3.selectAll("path").remove();
-    d3.selectAll("circle").remove();
-    d3.selectAll("text").remove();
-    d3.select("#info").html("");
-}
-
-//SAVE TO IMAGE
-//d3.select("#save").on("click", function(){
-//    var html = d3.select("svg")
-//        .attr("version", 1.1)
-//        .attr("xmlns", "http://www.w3.org/2000/svg")
-//        .node().parentNode.innerHTML;
-//
-//    console.log(html);
-//
-//    var img = '<img src="data:image/svg+xml;base64,'+ btoa(html)+'">';
-//    d3.select("#info").html(img);
-//});
-
+/*------------------helper functions--------------------- */
